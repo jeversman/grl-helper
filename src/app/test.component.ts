43970ddc91ec5b7715c8d4142907695d3f03cc97
Roman <romka_gorev@mail.ru>
2016-10-15 03:47:43 +0600

    add test.component, add progress bar

---
+++ src/app/test.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my-test',
+  templateUrl: './test.component.html'
+})
+
+export class TestComponent {
+  title: string = "test test test";
+}
